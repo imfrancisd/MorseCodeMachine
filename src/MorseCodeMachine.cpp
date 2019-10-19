ef093f48528c1ddf30e2a76cc37cdc70e21a598a
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-10-18 21:47:59 -0500

    Add the letters again.

--- src/MorseCodeMachine.cpp
+++ src/MorseCodeMachine.cpp
@@ -49,133 +49,133 @@ void sendMorse(const char message[], void (*dotDelayFunction)(), void (*dotFunct
                 DAH DAH DAH DAH DIT;
                 break;
 
-            case A:
-            case a:
+            case 'A':
+            case 'a':
                 DIT DAH;
                 break;
 
-            case B:
-            case b:
+            case 'B':
+            case 'b':
                 DAH DIT DIT DIT;
                 break;
 
-            case C:
-            case c:
+            case 'C':
+            case 'c':
                 DAH DIT DAH DIT;
                 break;
 
-            case D:
-            case d:
+            case 'D':
+            case 'd':
                 DAH DIT DIT;
                 break;
 
-            case E:
-            case e:
+            case 'E':
+            case 'e':
                 DIT;
                 break;
 
-            case F:
-            case f:
+            case 'F':
+            case 'f':
                 DIT DIT DAH DIT;
                 break;
 
-            case G:
-            case g:
+            case 'G':
+            case 'g':
                 DAH DAH DIT;
                 break;
 
-            case H:
-            case h:
+            case 'H':
+            case 'h':
                 DIT DIT DIT DIT;
                 break;
 
-            case I:
-            case i:
+            case 'I':
+            case 'i':
                 DIT DIT;
                 break;
 
-            case J:
-            case j:
+            case 'J':
+            case 'j':
                 DIT DAH DAH DAH;
                 break;
 
-            case K:
-            case k:
+            case 'K':
+            case 'k':
                 DAH DIT DAH;
                 break;
 
-            case L:
-            case l:
+            case 'L':
+            case 'l':
                 DIT DAH DIT DIT;
                 break;
 
-            case M:
-            case m:
+            case 'M':
+            case 'm':
                 DAH DAH;
                 break;
 
-            case N:
-            case n:
+            case 'N':
+            case 'n':
                 DAH DIT;
                 break;
 
-            case O:
-            case o:
+            case 'O':
+            case 'o':
                 DAH DAH DAH;
                 break;
 
-            case P:
-            case p:
+            case 'P':
+            case 'p':
                 DIT DAH DAH DIT;
                 break;
 
-            case Q:
-            case q:
+            case 'Q':
+            case 'q':
                 DAH DAH DIT DAH;
                 break;
 
-            case R:
-            case r:
+            case 'R':
+            case 'r':
                 DIT DAH DIT;
                 break;
 
-            case S:
-            case s:
+            case 'S':
+            case 's':
                 DIT DIT DIT;
                 break;
 
-            case T:
-            case t:
+            case 'T':
+            case 't':
                 DAH;
                 break;
 
-            case U:
-            case u:
+            case 'U':
+            case 'u':
                 DIT DIT DAH;
                 break;
 
-            case V:
-            case v:
+            case 'V':
+            case 'v':
                 DIT DIT DIT DAH;
                 break;
 
-            case W:
-            case w:
+            case 'W':
+            case 'w':
                 DIT DAH DAH;
                 break;
 
-            case X:
-            case x:
+            case 'X':
+            case 'x':
                 DAH DIT DIT DAH;
                 break;
 
-            case Y:
-            case y:
+            case 'Y':
+            case 'y':
                 DAH DIT DAH DAH;
                 break;
 
-            case Z:
-            case z:
+            case 'Z':
+            case 'z':
                 DAH DAH DIT DIT;
                 break;
 
