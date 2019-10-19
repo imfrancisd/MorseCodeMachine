eaa16532d169b64a7cd863633c84a90f659da2d9
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-10-18 21:43:39 -0500

    Add the letters.

--- src/MorseCodeMachine.cpp
+++ src/MorseCodeMachine.cpp
@@ -49,6 +49,136 @@ void sendMorse(const char message[], void (*dotDelayFunction)(), void (*dotFunct
                 DAH DAH DAH DAH DIT;
                 break;
 
+            case A:
+            case a:
+                DIT DAH;
+                break;
+
+            case B:
+            case b:
+                DAH DIT DIT DIT;
+                break;
+
+            case C:
+            case c:
+                DAH DIT DAH DIT;
+                break;
+
+            case D:
+            case d:
+                DAH DIT DIT;
+                break;
+
+            case E:
+            case e:
+                DIT;
+                break;
+
+            case F:
+            case f:
+                DIT DIT DAH DIT;
+                break;
+
+            case G:
+            case g:
+                DAH DAH DIT;
+                break;
+
+            case H:
+            case h:
+                DIT DIT DIT DIT;
+                break;
+
+            case I:
+            case i:
+                DIT DIT;
+                break;
+
+            case J:
+            case j:
+                DIT DAH DAH DAH;
+                break;
+
+            case K:
+            case k:
+                DAH DIT DAH;
+                break;
+
+            case L:
+            case l:
+                DIT DAH DIT DIT;
+                break;
+
+            case M:
+            case m:
+                DAH DAH;
+                break;
+
+            case N:
+            case n:
+                DAH DIT;
+                break;
+
+            case O:
+            case o:
+                DAH DAH DAH;
+                break;
+
+            case P:
+            case p:
+                DIT DAH DAH DIT;
+                break;
+
+            case Q:
+            case q:
+                DAH DAH DIT DAH;
+                break;
+
+            case R:
+            case r:
+                DIT DAH DIT;
+                break;
+
+            case S:
+            case s:
+                DIT DIT DIT;
+                break;
+
+            case T:
+            case t:
+                DAH;
+                break;
+
+            case U:
+            case u:
+                DIT DIT DAH;
+                break;
+
+            case V:
+            case v:
+                DIT DIT DIT DAH;
+                break;
+
+            case W:
+            case w:
+                DIT DAH DAH;
+                break;
+
+            case X:
+            case x:
+                DAH DIT DIT DAH;
+                break;
+
+            case Y:
+            case y:
+                DAH DIT DAH DAH;
+                break;
+
+            case Z:
+            case z:
+                DAH DAH DIT DIT;
+                break;
+
             default:
                 //Make everything else a delay between words.
                 //7 delays total. 3 from previous letter, 2 from here, 2 at end of loop.
