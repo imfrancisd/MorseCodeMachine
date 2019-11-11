7a4b7b9889a10017585ea8d8ec7270986d6b6f29
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-11-11 00:53:58 -0600

    Add seventh example.
    This example shows how to change the speed of the Morse code by modifying the
    behavior of the callback functions with local variables.

---
+++ examples/Example07-LocalSpeedControl/Example07-LocalSpeedControl.ino
@@ -0,0 +1,100 @@
+#include <MorseCodeMachine.h>
+
+void setup()
+{
+    //You are going to use the built-in LED in the Arduino to send Morse code.
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+    //The sendSlowly function below will send the Morse code slowly.
+    sendSlowly();
+    
+    //The sendQuickly function below will send the Morse code quickly.
+    sendQuickly();
+}
+
+void sendSlowly()
+{
+    //Send "<SOS>" in Morse code slowly.
+    //Create a local variable dotDuration and make it equal to 500.
+    //
+    //None of the other functions can read or write to this local variable
+    //unless you give the other functions a pointer to dotDuration.
+    //
+    //Notice the "&dotDuration". That is the pointer to dotDuration, and the
+    //pointer to dotDuration is given to the sendMorse function. The sendMorse
+    //function will then send that pointer to ledDelay, ledDot, and ledDash
+    //functions. How do I know? Because I wrote sendMorse to do that.
+
+    int dotDuration = 500;
+    sendMorse("<SOS>", ledDelay, ledDot, ledDash, &dotDuration);
+}
+
+void sendQuickly()
+{
+    //Send "<SOS>" in Morse code quickly.
+    //Modify the global variable dotDuration to equal 100.
+    //
+    //None of the other functions can read or write to this local variable
+    //unless you give the other functions a pointer to dotDuration.
+    //
+    //Notice the "&dotDuration". That is the pointer to dotDuration, and the
+    //pointer to dotDuration is given to the sendMorse function. The sendMorse
+    //function will then send that pointer to ledDelay, ledDot, and ledDash
+    //functions. How do I know? Because I wrote sendMorse to do that.
+
+    int dotDuration = 100;
+    sendMorse("<SOS>", ledDelay, ledDot, ledDash, &dotDuration);
+}
+
+void ledDelay(void *context)
+{
+    //Wait for a small amount of time.
+    //
+    //Notice that unlike the previous examples, the delay function now has
+    //a parameter "void *context". "void *" just means that the parameter is
+    //a pointer to any type of data. In this particular case, the pointer is
+    //given to ledDelay by the sendMorse function, which was given to the
+    //sendMorse function by the sendSlowly and sendQuickly functions, which is
+    //a pointer to an integer representing the dot duration.
+
+    //Extract the actual integer value from the void * pointer.
+
+    //First convert "void *context" to "int *pointerToInt" which is a pointer
+    //to an integer.
+    int *pointerToInt = static_cast<int *>(context);
+
+    //Then get the actual integer by using "*" in "*pointerToInt".
+    int dotDuration = *pointerToInt;
+
+    //Now, use that integer value to delay.
+    delay(dotDuration);
+}
+
+void ledDot(void *context)
+{
+    //Turn on the LED for a small amount of time, then turn off.
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    //You can also extract the integer value from "void *context" in one line.
+    delay(*static_cast<int *>(context));
+    
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
+void ledDash(void *context)
+{
+    //Turn on the LED three times as long as the dot, then turn off.
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    //Instead of extracting the integer value from "void *context" and calling
+    //delay, you can also call ledDelay and give ledDelay the "void *context".
+    ledDelay(context);
+    ledDelay(context);
+    ledDelay(context);
+
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
---
+++ examples/Example07-LocalSpeedControl/README.md
@@ -0,0 +1,15 @@
+# Example 7 - Local Speed Control
+
+Send Morse code at different speeds using the built-in LED in the Arduino.
+
+You will control the speed using local variables.
+
+# Y.O.U. Are Making This Hard
+
+Don't worry. You won't need to do this kind of thing until:
+
+* global variables become too difficult to manage
+* you want to use the sendMorse function, which was written in a C programming style, in classes and objects using a C++ programming style.
+
+Just know that this functionality is available for you in this library.
+
