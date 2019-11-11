de42d864aba194c43e4d3c9abf9b54782712d78b
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2019-11-11 01:47:47 -0600

    Add eighth example.
    This example shows how to use the sendMorse function, which was written with a
    C programming style, in a class using a C++ programming style.

---
+++ examples/Example08-OOMorseCode/Example08-OOMorseCode.ino
@@ -0,0 +1,81 @@
+#include <MorseCodeMachine.h>
+
+class LEDMorseCodeMachine
+{
+    public:
+    LEDMorseCodeMachine(int outputPin, int dotDuration)
+    {
+        _outputPin = outputPin;
+        _dotDuration = dotDuration;
+        pinMode(_outputPin, OUTPUT);
+    }
+    int ledDelay()
+    {
+        delay(_dotDuration);
+        return 0;
+    }
+    int ledDot()
+    {
+        digitalWrite(_outputPin, HIGH);
+        ledDelay();
+        digitalWrite(_outputPin, LOW);
+        return 0;
+    }
+    int ledDash()
+    {
+        digitalWrite(_outputPin, HIGH);
+        ledDelay();
+        ledDelay();
+        ledDelay();
+        digitalWrite(_outputPin, LOW);
+        return 0;
+    }
+    int ledSendMorse(const char message[])
+    {
+        //Unfortunately, you cannot use pointers to any class method like a
+        //pointer to a function.
+        //Fortunately, you can use a pointer to a static class method like a
+        //pointer to a function.
+        //So create static functions that call ledDelay, ledDot, and ledDash.
+        //That is what s_ledDelay, s_ledDot, and s_ledDash are for.
+        //The context will be the object calling ledSendMorse, so send "this"
+        //which is a pointer to the object calling ledSendMorse.
+
+        return sendMorse(message, s_ledDelay, s_ledDot, s_ledDash, this);
+    }
+    
+    private:
+    int _outputPin;
+    int _dotDuration;
+    
+    //These static methods are what bridges the C++ programming style with the
+    //C programming style of sendMorse(message, dly, dit, dah, context).
+    static int s_ledDelay(void *context)
+    {
+        return static_cast<LEDMorseCodeMachine *>(context)->ledDelay();
+    }
+    static int s_ledDot(void *context)
+    {
+        return static_cast<LEDMorseCodeMachine *>(context)->ledDot();
+    }
+    static int s_ledDash(void *context)
+    {
+        return static_cast<LEDMorseCodeMachine *>(context)->ledDash();
+    }
+};
+
+void setup()
+{
+}
+
+void loop()
+{
+    //Create a slow Morse code machine.
+    LEDMorseCodeMachine slow(LED_BUILTIN, 500);
+    slow.ledSendMorse("abc");
+    
+    //Create a quick Morse code machine.
+    LEDMorseCodeMachine quick(LED_BUILTIN, 100);
+    quick.ledSendMorse("it's easy as 1 2 3");
+}
+
---
+++ examples/Example08-OOMorseCode/README.md
@@ -0,0 +1,11 @@
+# Example 8 - OO Morse Code
+
+Use the sendMorse function, which was written using a C programming style, in
+a class using a C++ programming style.
+
+# Y.O.U. Are Making This Hard
+
+Don't worry.
+
+Just know that this functionality is available for you in this library.
+
